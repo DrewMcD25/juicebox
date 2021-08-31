595edcbc15baea55cd16b6d8b02a3b3d6e18c1c4
DrewMcD25 <drewmcd25@yahoo.com>
2021-08-30 21:26:03 -0500

    seeding

---
+++ db/seed.js
@@ -0,0 +1,54 @@
+const {
+    client,
+    getAllUsers // new
+  } = require('./index');
+  
+  async function testDB() {
+    try {
+      client.connect();
+  
+      const users = await getAllUsers();
+      console.log(users);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      client.end();
+    }
+  }
+
+  async function dropTables(){
+      try{
+          await client.query(`
+          `)
+      }catch (error){
+          throw error
+      }
+  }
+
+  async function createTables(){
+      try{
+          await client.query(`
+
+          `)
+      }catch (error){
+          throw error
+      }
+  }
+
+  async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      client.end();
+    }
+  }
+  
+  rebuildDB();
+
+
+testDB()
